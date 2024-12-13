163744e18c518ef3de3f27d2dbe2277b2cb9526c
OneMoreGate <143797131+OneMoreGate@users.noreply.github.com>
2024-12-13 19:29:27 +0300

    123

--- all_graphs.py
+++ all_graphs.py
@@ -12,13 +12,13 @@ def graph_DC_IV(data_path: str, save_path: str, name: str):
     fig, ax = plt.subplots(ncols=1, nrows=1, figsize = [10,5])
     ax.plot(df[0], np.abs(df[1]))
     ax.set_yscale('log')
-    ax.set_xlim([-3.2,5])
-    ax.set_ylim([10**(-12), 10**(-2)])
+    #ax.set_xlim([-5,5])
+    ax.set_ylim([10**(-8), 10**(-2)])
     #ax.set_title(f'{i} measurment with {lines[10].split()[1]} positive complines')
     ax.grid(which='major', linewidth = 0.6)
     ax.grid(which='minor', linewidth = 0.2)
-    ax.xaxis.set_major_locator(ticker.MultipleLocator(0.5))
-    ax.xaxis.set_minor_locator(ticker.MultipleLocator(0.1))
+    #ax.xaxis.set_major_locator(ticker.MultipleLocator(0.5))
+    #ax.xaxis.set_minor_locator(ticker.MultipleLocator(0.1))
     plt.savefig(save_path + f'\\{name}.png', dpi = 300, bbox_inches = 'tight')
     plt.clf()
     plt.close(fig)
@@ -30,7 +30,7 @@ def create_dir(path: str):
     else:
         os.mkdir(path)
 
-def all_graphs(data_path: str = 'hBN_1') :
+def all_graphs(data_path: str) :
     # чтение всех вложенных директорий и разделение на файлы и папки
     dir_list = []
     file_list = []
@@ -51,15 +51,15 @@ def all_graphs(data_path: str = 'hBN_1') :
     all_contacts_info = {}
     for i in header_list:
         with open(f'{data_path}\{i}') as f:
-            claen_header = []
+            header_lines = []
             for k in f.readlines()[:-2]:
-                claen_header.append(k.replace('\n', ''))
+                header_lines.append(k.replace('\n', ''))
             # информация о всех отдельных измерениях на одном контакте
             single_contact_info = {}
-            for j in range(len(claen_header)):
-                if claen_header[j] == splite_line:
-                    number, *m_type = claen_header[j + 3].split()
-                    file_path = claen_header[j + 8].split()[-1]
+            for j in range(len(header_lines)):
+                if header_lines[j] == splite_line:
+                    number, *m_type = header_lines[j + 3].split()
+                    file_path = header_lines[j + 8].split()[-1]
                     single_contact_info[int(number)] = {'type': ' '.join(m_type), 'file_path': file_path}
                 else:
                     continue
@@ -81,3 +81,4 @@ def all_graphs(data_path: str = 'hBN_1') :
             match all_contacts_info[dir_list[i]][data_number]['type']:
                 case 'DC IV':
                     graph_DC_IV(local_data_path, local_graph_path, str(data_number))
+
